268038425d3b044222977712339edaff63a01c60
damchm <damian.chmielewski.ext@iceservices.com>
2022-07-19 15:15:27 +0200

    first commit

---
+++ src/Loop.java
@@ -0,0 +1,11 @@
+public class Loop {
+    public static void main(String[] args) {
+//pętle
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("to jest pętla");
+            System.out.println("aktualna wartość licznika: " +i);
+        }
+        System.out.println("Koniec pętli");
+    }
+}
